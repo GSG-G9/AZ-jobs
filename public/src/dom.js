4f2baa0299e367a60ca5aadbb1e29a8e0177c0ac
zeinjendeya <z.jendeya@gmail.com>
2020-12-02 17:04:34 +0200

    fixing code

--- public/src/dom.js
+++ public/src/dom.js
@@ -1,30 +1,28 @@
 const searchInput = document.getElementById("search_input");
 const searchBtn = document.getElementById("btnSearch");
 const searchResultDiv = document.getElementById("search_result");
-let url = new URL("/jobs", "http://localhost:3000");
-
-function getInputValue() {
-  let inputValue = searchInput.value;
-  return inputValue;
-}
-
-function clearContainer() {
-  searchResultDiv.textContent = "";
-}
 
 function createSearchElement() {
-  let searchValue = getInputValue();
-  let paramsString = `q= ${searchValue}`;
-  url.search = new URLSearchParams(paramsString);
-  fetch(url)
+  const searchValue = searchInput.value.trim();
+  const paramsString = `/jobs?q=${searchValue}`;
+  if (searchValue.length === 0) {
+    const errMsg = document.createElement("p");
+    errMsg.textContent = "cannot be empty!";
+    searchResultDiv.appendChild(errMsg);
+  }else{
+    fetch(paramsString)
     .then((result) => result.json())
     .then((response) => {
-      for (let i = 0; i < response.length; i++) {
-        let job = response[i].suggestion;
-        const jobElement = document.createElement("p");
-        jobElement.setAttribute("class", "job-title");
-        jobElement.textContent = job;
-        searchResultDiv.appendChild(jobElement);
+      if (response.error) {
+        throw new Error(response["error"]["message"]);
+      } else {
+        for (let i = 0; i < response.length; i++) {
+          let job = response[i].suggestion;
+          const jobElement = document.createElement("p");
+          jobElement.setAttribute("class", "job-title");
+          jobElement.textContent = job;
+          searchResultDiv.appendChild(jobElement);
+        }
       }
     })
     .catch((err) => {
@@ -32,18 +30,12 @@ function createSearchElement() {
       msg.textContent = err.message;
       searchResultDiv.appendChild(msg);
     });
-}
-function msgError() {
-  if (searchInput.length === 0) {
-    const errMsg = document.createElement("p");
-    errMsg.textContent = "cannot be empty!";
-    searchResultDiv.appendChild(errMsg);
-  } else {
-    createSearchElement();
   }
+  
 }
 
+
 searchBtn.addEventListener("click", () => {
-  clearContainer();
-  msgError();
+  searchResultDiv.textContent = "";
+  createSearchElement();
 });
